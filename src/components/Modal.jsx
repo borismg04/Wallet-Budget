36e4b2267949efee71b6d75da9e446f532b56887
bmonroyg <bmonroyg@emeal.nttdata.com>
2022-09-26 16:34:12 -0500

    Modal Complete

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,13 @@
 import iconoCerrar from '../img/cerrar.svg'
 
-const Modal = ({setModal}) => {
+const Modal = ({setModal,animarModal,setAnimarModal}) => {
 
   const ocultarModal = () => {
-    setModal(false);
+    setAnimarModal(false);
+
+    setTimeout(() => {
+      setModal(false);
+    }, 500);
   }
 
   return (
@@ -16,8 +20,48 @@ const Modal = ({setModal}) => {
         />
       </div>
 
-      <form>
+      <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
         <legend>Nuevo Gasto</legend>
+
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+          <input
+            type="text"
+            id="nombre"
+            placeholder="Añade el Nombre del Gasto"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+          <input
+            type="number"
+            id="cantidad"
+            placeholder="Añade la Cantidad del Gasto"
+          />
+        </div>
+        
+        <div className="campo">
+          <label htmlFor="categoria">Categoria</label>
+          <select
+            id='categoria'
+          >
+            <option value="">-- Seleccione --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="Suscripciones">Suscripciones</option>
+          </select>
+        </div>
+
+        <input
+          type="submit"
+          value="Añadir Gasto"
+        />
+
       </form>
     </div>
   )
