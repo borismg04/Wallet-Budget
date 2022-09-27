21432e104d98fe63e1a90aff91ed7b5e6dddd811
bmonroyg <bmonroyg@emeal.nttdata.com>
2022-09-27 15:44:07 -0500

    style spent

--- src/components/Spent.jsx
+++ src/components/Spent.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
+import { formatearFecha } from '../helpers';
 
 const Spent = ({gasto}) => {
 
-  const {categoria,nombre,cantidad,id}= gasto;
+  const {categoria,nombre,cantidad,id,fecha}= gasto;
 
   return (
     <div className='gasto sombra'>
       <div className='contenido-gasto'>
-
+        {/*IMAGEN*/}
         <div className='descripcion-gasto'>
           <p className='categoria'>{categoria}</p>
           <p className='nombre-gasto'>{nombre}</p>
-
+          <p className='fecha-gasto'>
+            Agregado el: {''}
+            <span>{formatearFecha(fecha)}</span>
+          </p>
         </div>
       </div>
+        <p className='cantidad-gasto'>${' '}{cantidad}</p>
     </div>
   )
 }
